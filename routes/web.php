99fe182d01a408111894ac0b28ae00e6be14f2e4
Sparclex <silvan.wehner@gmail.com>
2018-09-03 17:51:09 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Nathanmac\Utilities\Parser\Facades\Parser;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::redirect('/', '/nova');
 
 Route::get('/test', function() {
     return collect(Parser::xml(file_get_contents(storage_path('app/rdml.xml'))));
