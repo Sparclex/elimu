3ee6ed3dd436250d273d0b40a702630589b45388
Sparclex <silvan.wehner@gmail.com>
2019-03-01 12:46:03 +0100

    Change sidebar order

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -30,12 +30,12 @@
         </li>
         <li class="leading-tight mb-4 ml-8 text-sm">
             <router-link :to="{name: 'index', params: {resourceName: 'assay-definition-files'}}" class="text-white text-justify no-underline dim">
-                Assay Definition Files
+                Definition Files
             </router-link>
         </li>
         <li class="leading-tight mb-4 ml-8 text-sm">
             <router-link :to="{name: 'index', params: {resourceName: 'primer-mixes'}}" class="text-white text-justify no-underline dim">
-                Primer Mixes
+                Master Mixes
             </router-link>
         </li>
         <li class="leading-tight mb-4 ml-8 text-sm">
@@ -53,30 +53,31 @@
                 Protocols
             </router-link>
         </li>
-        <li class="leading-tight mb-4 ml-8 text-sm">
-            <router-link :to="{name: 'index', params: {resourceName: 'instruments'}}" class="text-white text-justify no-underline dim">
-                Instruments
-            </router-link>
-        </li>
-        <li class="leading-tight mb-4 ml-8 text-sm">
-            <router-link :to="{name: 'index', params: {resourceName: 'maintenances'}}" class="text-white text-justify no-underline dim">
-                Maintenances
-            </router-link>
-        </li>
     </ul>
 
 
     <h3 class="flex items-center font-normal text-white mb-6 text-base no-underline">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--sidebar-icon)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sidebar-icon"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg> <span class="sidebar-label">Results</span></h3>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--sidebar-icon)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sidebar-icon"><ellipse cx="12" cy="5" rx="9" ry="3"></ellipse><path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path><path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path></svg> <span class="sidebar-label">Results</span></h3>
     <ul class="list-reset mb-8">
         <li class="leading-tight mb-4 ml-8 text-sm">
-            <router-link :to="{name: 'results'}" class="text-white text-justify no-underline dim">
-                Overview
+            <router-link :to="{name: 'index', params: {resourceName: 'experiments'}}" class="text-white text-justify no-underline dim">
+                Experiments
             </router-link>
         </li>
         <li class="leading-tight mb-4 ml-8 text-sm">
-            <router-link :to="{name: 'lens', params: {resourceName: 'results', lens: 'invalid'}}" class="text-white text-justify no-underline dim">
-                Invalid
+            <router-link :to="{name: 'results'}" class="text-white text-justify no-underline dim">
+                Results
+            </router-link>
+        </li>
+    </ul>
+
+    <h3 class="flex items-center font-normal text-white mb-6 text-base no-underline">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--sidebar-icon)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sidebar-icon"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg> <span class="sidebar-label">QC</span></h3>
+    <ul class="list-reset mb-8">
+
+        <li class="leading-tight mb-4 ml-8 text-sm">
+            <router-link :to="{name: 'index', params: {resourceName: 'maintenances'}}" class="text-white text-justify no-underline dim">
+                Maintenances
             </router-link>
         </li>
         <li class="leading-tight mb-4 ml-8 text-sm">
