95534696302a082be29dafd8d5e02896309d092a
Sparclex <silvan.wehner@gmail.com>
2019-03-29 11:30:45 +0100

    allow configuration of seperate telescope db

--- config/database.php
+++ config/database.php
@@ -59,6 +59,21 @@ return [
             'engine' => null,
         ],
 
+        'telescope' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('TELESCOPE_DB_DATABASE', 'forge'),
+            'username' => env('TELESCOPE_DB_USERNAME', 'forge'),
+            'password' => env('TELESCOPE_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
